62c0ab7fcfae0f372d86b4cfb2259a55aab447ad
Kailash Patel <patelkailash200@gmail.com>
2023-08-02 22:28:30 +0530

    login with number component change, npm react-phone-input-2 install

--- src/component/LoginWithNumber.js
+++ src/component/LoginWithNumber.js
@@ -1,15 +1,60 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
-
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/style.css'
+import AlertContext from '../context/AlertContext'
 const LoginWithNumber = () => {
+  const { setAlert } = useContext(AlertContext)
+  //send otp
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [name, setName] = useState('')
+  const [otpSent, setOtpSent] = useState(false)
+  const handleSendOtp = (e) => {
+    e.preventDefault()
+    const sendData = {
+      name: name,
+      phoneNumber: phoneNumber
+    }
+    console.log('sendData', sendData)
+    const canSend = Object.values(sendData).every(Boolean)
+    if (canSend) {
+      setOtpSent(true)
+      setName('')
+      setPhoneNumber('')
+    } else {
+      setAlert({ show: true, message: "Please enter name and phone number" })
+    }
+  }
+  //verify otp
+  const otpInitial = {
+    otpDigit1:'',
+    otpDigit2:'',
+    otpDigit3:'',
+    otpDigit4:''
+  }
+  const [otpInputs, setOtpInputs] = useState(otpInitial)
+  const handleOtpChange = (e)=>{
+    e.preventDefault()
+    setOtpInputs({...otpInputs,[e.target.name]:e.target.value})
+  }
+  const canVerify = Object.values(otpInputs).every(Boolean)
+  const handleVerifyOtp = (e)=>{
+    e.preventDefault()
+    console.log('otp--->',otpInputs)
+    if(canVerify){
+      setOtpInputs(otpInitial)
+    }else{
+      setAlert({ show: true, message: "Please enter valid otp" })
+    }
+  }
   return (
     <div className="container d-flex flex-column justify-content-center align-items-center my-5">
-    <div className="shadow-lg rounded p-3">
-      <div className="text-center mb-2 mt-1 fs-4">
+      <div className="shadow-lg rounded p-3">
+        <div className="text-center mb-2 mt-1 fs-4">
           login to start session
-      </div>
-      <form>
-      <div className="mb-2">
+        </div>
+        <form>
+          <div className="mb-2">
             <label htmlFor="fullName" className="form-label">
               Full Name
             </label>
@@ -19,17 +64,20 @@ const LoginWithNumber = () => {
               id="name"
               placeholder="Full Name"
               name="name"
+              required={true}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
-        <div className="mb-2">
-          <label htmlFor="phoneNumber" className="form-label">
-           Phone number
-          </label>
-            <div className='d-flex  justify-content-between align-items-center gap-2'>
+          <div className="mb-2">
+            <label htmlFor="phoneNumber" className="form-label">
+              Phone number
+            </label>
+            {/* <div className='d-flex  justify-content-between align-items-center gap-2'>
             <select name="country" id="country" className='form-select  w-50'>
-                <option value="+91">India (+91)</option>
-                <option value="+92">India2 (+92)</option>
-                <option value="+93">India3 (+93)</option>
+                <option value="+91">IN (+91)</option>
+                <option value="+92">AU(+92)</option>
+                <option value="+93">US (+93)</option>
             </select>
             <input
             type="number"
@@ -39,73 +87,101 @@ const LoginWithNumber = () => {
             max={10}
            placeholder="Enter valid number" name="phoneNumber"
           />
+            </div> */}
+            <PhoneInput
+              country={'in'}
+              value={phoneNumber}
+              onChange={(number) => setPhoneNumber(number)}
+              inputProps={{
+                name: 'phoneNumber',
+                required: true,
+                class: 'form-control',
+                autoFocus: true,
+              }}
+              inputStyle={{
+                width: "100%",
+              }}
+              dropdownStyle={{
+                color: 'black'
+              }}
+            />
+          </div>
+          <div className="row px-2">
+            <button onClick={handleSendOtp} className="btn btn-primary ">
+              Send Otp
+            </button>
+          </div>
+          <div className='mt-4'>
+            <div className="mb-2">
+              <label htmlFor="otp" className="form-label">
+                Enter otp sent on above phone number
+              </label>
+              <div className='d-flex justify-content-around align-items-center gap-3'>
+                <input
+                  type="number"
+                  className="p-1 text-center shadow-none rounded form-control"
+                  min={1}
+                  max={1}
+                  name="otpDigit1"
+                  id='otpDigit1'
+                  disabled={!otpSent}
+                  value={otpInputs.otpDigit1}
+                  onChange={handleOtpChange}
+                />
+                <input
+                  type="number"
+                  className="p-1 text-center shadow-none rounded form-control"
+                  min={1}
+                  max={1}
+                  name="otpDigit2"
+                  id='otpDigit2'
+                  disabled={!otpSent}
+                  value={otpInputs.otpDigit2}
+                  onChange={handleOtpChange}
+                />
+                <input
+                  type="number"
+                  className="p-1 text-center shadow-none rounded form-control"
+                  min={1}
+                  max={1}
+                  name="otpDigit3"
+                  id='otpDigit3'
+                  disabled={!otpSent}
+                  value={otpInputs.otpDigit3}
+                  onChange={handleOtpChange}
+                />
+                <input
+                  type="number"
+                  className="p-1 text-center shadow-none rounded form-control"
+                  min={1}
+                  max={1}
+                  name="otpDigit4"
+                  id='otpDigit4'
+                  disabled={!otpSent}
+                  value={otpInputs.otpDigit4}
+                  onChange={handleOtpChange}
+                />
+              </div>
             </div>
+            <div className='row px-2 mt-2 mb-2'>
+              <button type='button' onClick={handleVerifyOtp} disabled={!otpSent || !canVerify} className="btn btn-success">
+                Verify otp
+              </button>
+            </div>
+            <div className="d-flex justify-content-end mt-1">
+              <button disabled={!otpSent || !canVerify} className="btn btn-sm btn-outline-primary">
+                send again?
+              </button>
+            </div>
+          </div>
+        </form>
+        <div className="text-center float-end mt-1">
+          <Link className="text-decoration-none text-secondary" to="/login">
+            Other ways to start
+          </Link>
         </div>
-        <div className='mb-2'>
-
-        </div>
-        <div className="row px-2">
-          <button   className="btn btn-primary ">
-            Send Otp
-          </button>
-        </div>
-        <div className='mt-3'>
-        <div className="mb-2">
-          <label htmlFor="phoneNumber" className="form-label">
-           Enter otp sent on phone number
-          </label>
-         <div className='d-flex justify-content-around align-items-center gap-3'>
-         <input
-            type="number"
-            className="p-1 text-center shadow-none rounded form-control"
-            id="phoneNumber"
-            min={1}
-            max={1}
-            name="otpDigit1"
-          />
-           <input
-            type="number"
-            className="p-1 text-center shadow-none rounded form-control"
-            id="phoneNumber"
-            min={1}
-            max={1}
-            name="otpDigit2"
-          />
-           <input
-            type="number"
-            className="p-1 text-center shadow-none rounded form-control"
-            id="phoneNumber"
-            min={1}
-            max={1}
-            name="otpDigit3"
-          />
-           <input
-            type="number"
-            className="p-1 text-center shadow-none rounded form-control"
-            id="phoneNumber"
-            min={1}
-            max={1}
-            name="otpDigit4"
-          />
-         </div>
-        </div>
-        <div className='row px-2 mt-2 mb-2'>
-        <button type='button' className="btn btn-success">
-            Verify otp
-          </button>
-        </div>
-        </div>
-      </form>
-      <form>
-
-      </form>
-      <div className="text-center float-end mt-1">
-            <Link className="text-decoration-none text-secondary" to="/login">
-                Other ways to start
-            </Link>
-        </div>
+      </div>
     </div>
-  </div>
   )
 }
 
