bce44dbd63cb086b8fdf70fcac7a34b077808ade
Kailash Patel <patelkailash200@gmail.com>
2023-08-08 22:07:45 +0530

    single product design change

--- src/component/SingleProduct.js
+++ src/component/SingleProduct.js
@@ -1,48 +1,85 @@
-import {React} from 'react'
+import { React } from 'react'
 import { useParams } from 'react-router-dom'
 import FormatPrice from './FormatPrice';
 import { useGetProductsQuery } from '../reducers/productsSlice';
 
 function SingleProduct() {
-     const {productId} = useParams()
-    //  console.log('params id',productId)
-     const {product} = useGetProductsQuery('getProducts',{
-      selectFromResult:({data})=>({
-        product:data?.entities[productId],
-        // isLoading
-      })
-    }) 
-  console.log('single product',product)
+  const { productId } = useParams()
+  //  console.log('params id',productId)
+  const { product } = useGetProductsQuery('getProducts', {
+    selectFromResult: ({ data }) => ({
+      product: data?.entities[productId],
+      // isLoading
+    })
+  })
+  console.log('single product', product)
   // if(isLoading){
   //   return(<p>Loading...</p>)
   // }
-  if(!product){
+  if (!product) {
     return (<p>{`No Product with ID ${productId}`}</p>);
   }
   return (
     <>
-    <div className='container-fluid mt-3 mb-3'>
-    {product &&     <div className="card" style={{ width: "200px" }}>
-          <div className="text-center mt-1">
-            <img
-              src={product.image}
-              alt={product.title}
-              height={200}
-              className="card-img-top"
-            />
-          </div>
-          <div className="card-body">
-            <div className="d-flex justify-content-between">
-            <p className="card-text">iphone</p>
-            <p className="card-text"><FormatPrice price={product.price} /></p>
-            </div>
-            <div className="d-flex flex-nowrap">
-                <p className="card-text text-truncate w-75">{product.description}</p>
-                <p className="card-text ">{product.rating}<span><i className="bi bi-star-fill"></i> </span></p>
+      <div className='container-fluid mt-5 mb-3'>
+        <div className='product text-center row'>
+          <div className='imagesDiv col'>
+            <div className="images row ms-5 me-5">
+              <div className="images col">
+                <div className='row w-50 g-2'>
+                  <img src={product.image} alt={product.title} height={100} className='img object-fit-fill bg-transparent' />
+                  <img src={product.image} alt={product.title} height={100} className='img object-fit-fill bg-transparent' />
+                  <img src={product.image} alt={product.title} height={100} className='img object-fit-fill bg-transparent' />
+                  <img src={product.image} alt={product.title} height={100} className='img object-fit-fill bg-transparent' />
+                </div>
+              </div>
+              <div className="image col text-center m-auto ">
+                <img src={product.image} alt={product.title}  className='img-fluid object-fit-fill bg-transparent' />
+              </div>
             </div>
           </div>
-        </div>}
-    </div>
+          <div className='productDetailsDiv col text-start'>
+            <div className='fw-bold fs-4 mb-2'>
+              {product.title}
+            </div>
+            <div className='fw-lighter mb-2'>
+              {product.description}
+            </div>
+            <div className='mb-1'>
+              Category: {product.categoryName}
+            </div>
+            <div className='mb-1'>
+              Rating : {product.rating}<span><i className="bi bi-star-fill ms-1"></i> </span>
+            </div>
+            <div className='mb-1'>
+             Available : In Stock
+            </div>
+            <div className='fw-bold fs-5 mb-1'>
+              Price : <FormatPrice price={product.price} />
+            </div>
+            <div className='d-flex align-items-start gap-3'>
+              <button className='btn btn-primary'>Buy Now</button>
+              <button className='btn btn-secondary'>Add To Chart</button>
+            </div>
+            <div className='w-50'><hr /></div>
+            <div className='d-flex gap-2'>
+              <div className='text-center d-flex flex-column'>
+                <i className="bi bi-truck"></i>
+                <span className='fw-lighter'>Free Delivery</span>
+              </div>
+              <div className='text-center d-flex flex-column' >
+                  <i class="bi bi-currency-rupee"></i>
+                <span  className='fw-lighter'>Pay On Delivery</span>
+              </div>
+              <div className='text-center d-flex flex-column'>
+                <i class="bi bi-shield-check"></i>
+                <span  className='fw-lighter'>1 Warranty</span>
+              </div>
+            </div>
+            <div className='w-50'><hr /></div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
