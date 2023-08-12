c37c8c830d61415ec9a148cb18c82075214d448d
Kailash Patel <patelkailash200@gmail.com>
2023-08-12 21:37:27 +0530

    cart and cartITem code updated

--- src/component/Cart.js
+++ src/component/Cart.js
@@ -1,24 +1,29 @@
-import React, { useContext} from "react"
+import React, { useContext, useEffect, useState } from "react"
 import CartItem from "./CartItem"
 import CartContext from "../context/CartContext"
 import { useGetProductsQuery } from "../reducers/productsSlice"
 function Cart() {
-  const {cart} = useContext(CartContext)
-  const {data:products,isLoading,isSuccess,isError,error} = useGetProductsQuery('getProducts')
-  console.log('products',products)
-  let cartProducts = []
-  let totalAmount = 0
-  if(isSuccess){
-    cartProducts =  cart.map((productId)=>{
-      return products.entities[productId]
-    })
-    totalAmount = cartProducts.reduce((sum,obj)=>{
-      return sum + parseFloat(obj.price)
-    },0)
-  }
-  
-  console.log('cart data in cart',cart)
-  console.log('products in cart',cartProducts)
+  const { cart } = useContext(CartContext)
+  const { data: products, isSuccess } = useGetProductsQuery('getProducts');
+  const [cartProducts, setCartProducts] = useState([]);
+
+  useEffect(() => {
+    if (isSuccess && cart.length > 0 && cartProducts.length === 0) {
+      const fetchedProducts = cart.map((productId) => products.entities[productId]);
+      setCartProducts(fetchedProducts);
+    }
+  }, [isSuccess, cart, products, cartProducts.length]);
+  const handleItemUpdate = (data) => {
+    const updatedCartProducts = cartProducts.map((product) => {
+      if (product.id === data.id) {
+        return { ...product, quantity: parseInt(data.quantity) };
+      }
+      return product;
+    });
+    // this created infinte re-rendering issue on cart but now fixed may be code updated in both cart and cartItem with useEffect
+    setCartProducts(updatedCartProducts);
+  };
+  console.log('cartProducts', cartProducts)
   return (
     <>
       <div className="container">
@@ -26,16 +31,16 @@ function Cart() {
           <div className="col col-md-9 ">
             <h2 className="heading">Shopping Cart</h2>
             <div className="products">
-             {cartProducts &&
-             cartProducts.map((product)=>{
-              return(
-                <CartItem key={product.id} productId={product.id} />
-              )
-             })
-             }
+              {cartProducts &&
+                cartProducts.map((product) => {
+                  return (
+                    <CartItem key={product.id} productId={product.id} handleItemUpdate={handleItemUpdate} />
+                  )
+                })
+              }
             </div>
             <div className="subTotal float-end border-top">
-              Subtotal (  {cartProducts.length} Items ) : {totalAmount}
+              Subtotal (  {cartProducts.length} Items ) : {0}
             </div>
           </div>
           <div className="col col-md-3 mt-5 mb-2">
@@ -44,7 +49,7 @@ function Cart() {
                 Your order is eligible for FREE Delivery.
               </div>
               <div className="card-body">
-                <div className="card-title"> Subtotal ( {cartProducts.length} Items ) : {totalAmount}</div>
+                <div className="card-title"> Subtotal ( {cartProducts.length} Items ) : {0}</div>
                 <p className="card-text"></p>
                 <div className="text-center">
                   <a href="/buy" className="btn btn-primary">
--- src/component/CartItem.js
+++ src/component/CartItem.js
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useGetProductsQuery } from '../reducers/productsSlice'
+import FormatPrice from './FormatPrice'
+import CartContext from '../context/CartContext'
 
-function CartItem({productId}) {
+function CartItem({productId,handleItemUpdate}) {
+  const {cart,setCart} = useContext(CartContext)
   const { product } = useGetProductsQuery('getProducts', {
     selectFromResult: ({ data }) => ({
       product: data?.entities[productId],
     })
   })
+  const [quantity, setQuantity] = useState(1);
+
+  const handleMinusQuantity = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const handlePlusQuanity = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const handleCartItemDelete = () => {
+    const updatedCart = cart.filter(item => item.id !== productId);
+    setCart(updatedCart);
+  };
+
+  useEffect(() => {
+    handleItemUpdate({
+      id: product.id,
+      price: product.price,
+      quantity: quantity,
+    });
+  }, [quantity]);
   return (
     <div className="product d-flex justify-content-center card mb-3">
     <div className="row g-0">
@@ -30,10 +57,16 @@ function CartItem({productId}) {
             Free Delivery
           </p>
           <p className="card-text rating">Rating: 5</p>
-          <div className="d-flex gap-3 justify-content-start align-items-center">
-            <p className="card-text price">Price : {product.price}</p>
-            <p className="card-text delete">Delete</p>
+          <div className="row align-items-center border mb-2">
+            <p className="col align-self-center"><span>Price : </span> <FormatPrice price={product.price} /></p>
+            <div className='col align-self-center'>
+            <span>Quantity : </span>
+            <span className='btn rounded' onClick={handleMinusQuantity}><i className='bi bi-dash-circle-fill'></i></span>
+              <label >{quantity}</label>
+            <span className='btn rounded' onClick={handlePlusQuanity}><i className='bi bi-plus-circle-fill'></i></span>
+            </div>
           </div>
+          <button className="delete col btn btn-outline-danger" onClick={handleCartItemDelete}>Delete</button>
         </div>
       </div>
     </div>
